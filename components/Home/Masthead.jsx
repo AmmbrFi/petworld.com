168bd13b28dac6365b72fc3566ba97a4b4982118
Arsha <arshaskrd97@gmail.com>
2022-01-17 15:17:08 +0530

    Remove contents

--- components/Home/Masthead.jsx
+++ components/Home/Masthead.jsx
@@ -4,11 +4,8 @@ import React from 'react'
 const Masthead = props => {
   return (
     <section className="flex flex-col items-center justify-center w-full md:w-5/12 mx-auto text-center h-[400px]">
-      <h1 className="font-serif text-6xl">Pet world park</h1>
-      <p className="text-xl my-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-        purus sit amet luctus venenatis, lectus
-      </p>
+      <h1 className="font-serif text-6xl my-4">Pet world park</h1>
+
       <Link href="/contact">
         <a className="btn">Contact</a>
       </Link>
