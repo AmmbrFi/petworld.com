ddcae9ef267739905d126d56547180ffe5f74e4b
Arsha <arshaskrd97@gmail.com>
2022-01-19 17:42:50 +0530

    Change font color

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,11 +7,11 @@ const Contact = props => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="w-9/12">
             <h2>Contact Us</h2>
-            <p className="text-lg leading-relaxed text-gray-500 mt-4">
+            <p className="text-lg leading-relaxed text-gray-700 mt-4">
               We are always pleased to receive messages, even if its just to
               tell us you like our work or to tell us about your pet.
             </p>
-            <p className="text-lg leading-relaxed text-gray-500 mt-4">
+            <p className="text-lg leading-relaxed text-gray-700 mt-4">
               Feel free to also contact us about partnership opportunities in
               your business.
             </p>
