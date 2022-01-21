a33fcca13adc5b1742111758bfa3a80814c857d7
cbammbr <sibiraj@ammbrtech.com>
2022-01-21 15:15:47 +0530

    Update text

--- components/Detail/NftDetail.jsx
+++ components/Detail/NftDetail.jsx
@@ -18,12 +18,12 @@ const NFTDetail = () => {
 
             <div className="flex flex-col my-4 space-y-5">
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
+                {`Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged.
+                electronic typesetting, remaining essentially unchanged.`}
               </p>
               <p className="text-black text-2xl">1600 BUSD</p>
             </div>
--- components/Marketplace/NftList.jsx
+++ components/Marketplace/NftList.jsx
@@ -6,14 +6,14 @@ const NftList = () => {
     <div className="container px-4 my-20">
       <p className="text-xl font-semibold text-black mb-5"> Items</p>
       <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
-        <NftCard {...{ path: `https://placeimg.com/305/300/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/300/300/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/295/305/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/290/305/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/285/305/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/280/305/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/310/305/animals` }} />
-        <NftCard {...{ path: `https://placeimg.com/315/305/animals` }} />
+        <NftCard {...{ path: 'https://placeimg.com/305/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
+        <NftCard {...{ path: 'https://placeimg.com/300/300/animals' }} />
       </div>
     </div>
   )
