d800d0457da95dd9fc511fb7c4f0464f6dad7a38
Arsha <arshaskrd97@gmail.com>
2022-02-01 14:17:25 +0530

    Fix error

--- components/Detail/NftDetail.jsx
+++ components/Detail/NftDetail.jsx
@@ -8,14 +8,14 @@ const NFTDetail = () => {
           <div className="bg-orange rounded-full md:p-4 p-1 cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="white">
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokewidth="2"
                 d="M15 19l-7-7 7-7"
               />
             </svg>
@@ -51,14 +51,14 @@ const NFTDetail = () => {
           <div className="bg-orange rounded-full md:p-4 p-1 cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="white">
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M9 5l7 7-7 7"
               />
             </svg>
