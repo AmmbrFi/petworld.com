7f57cda7bfef2ff5eda7c93faff011744eb380e7
Arsha <arshaskrd97@gmail.com>
2022-02-01 16:00:11 +0530

    Add state

--- components/Detail/Tags.jsx
+++ components/Detail/Tags.jsx
@@ -1,19 +1,46 @@
+import React, { useState } from 'react'
+
 import Link from 'next/link'
-import React from 'react'
 
 const Tags = props => {
+  const [display, setDisplay] = useState(1)
+  console.log('$$$', display)
   return (
     <div className="flex flex-row md:space-x-6 space-x-2">
-      <div className="bg-orange rounded-full md:px-10 md:py-2 px-3 cursor-pointer">
+      <div
+        onClick={() => setDisplay(1)}
+        className={` rounded-full md:px-10 md:py-2 px-3  cursor-pointer ${
+          display === 1
+            ? 'bg-[#F08A4B] text-white active'
+            : 'bg-gray-200 text-black'
+        }`}>
         All
       </div>
-      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black cursor-pointer">
+      <div
+        onClick={() => setDisplay(2)}
+        className={` rounded-full md:px-10 md:py-2 px-3  cursor-pointer ${
+          display === 2
+            ? 'bg-[#F08A4B] text-white active'
+            : 'bg-gray-200 text-black'
+        }`}>
         Cats
       </div>
-      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black cursor-pointer">
+      <div
+        onClick={() => setDisplay(3)}
+        className={`rounded-full md:px-10 md:py-2 px-3  cursor-pointer ${
+          display === 3
+            ? 'bg-[#F08A4B] text-white active'
+            : 'bg-gray-200 text-black'
+        }`}>
         Dogs
       </div>
-      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black cursor-pointer">
+      <div
+        onClick={() => setDisplay(4)}
+        className={`rounded-full md:px-10 md:py-2 px-3  cursor-pointer ${
+          display === 4
+            ? 'bg-[#F08A4B] text-white active'
+            : 'bg-gray-200 text-black'
+        }`}>
         Parrots
       </div>
     </div>
