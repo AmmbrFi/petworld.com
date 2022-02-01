e4d64cda5178077015d1b13ff0846c0c5a4e7b8f
Arsha <arshaskrd97@gmail.com>
2022-02-01 15:03:27 +0530

    Add cursor pointer

--- components/Detail/Tags.jsx
+++ components/Detail/Tags.jsx
@@ -4,14 +4,16 @@ import React from 'react'
 const Tags = props => {
   return (
     <div className="flex flex-row md:space-x-6 space-x-2">
-      <div className="bg-orange rounded-full md:px-10 md:py-2 px-3">All</div>
-      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black">
+      <div className="bg-orange rounded-full md:px-10 md:py-2 px-3 cursor-pointer">
+        All
+      </div>
+      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black cursor-pointer">
         Cats
       </div>
-      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black">
+      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black cursor-pointer">
         Dogs
       </div>
-      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black">
+      <div className="bg-gray-200 rounded-full md:px-10 md:py-2 px-3 text-black cursor-pointer">
         Parrots
       </div>
     </div>
