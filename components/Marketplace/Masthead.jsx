38fdd9692a4dd1d47481bacb50ef086fce27aa95
Arsha <arshaskrd97@gmail.com>
2022-02-01 14:10:28 +0530

    Change name

--- components/Marketplace/Masthead.jsx
+++ components/Marketplace/Masthead.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Masthead = props => {
   return (
     <section className="flex flex-col items-center justify-center w-full md:w-full mx-auto text-center">
-      <h1 className="font-serif md:text-5xl text-4xl">NFT Store</h1>
+      <h1 className="font-serif md:text-5xl text-4xl">Membership NFT Store</h1>
     </section>
   )
 }
