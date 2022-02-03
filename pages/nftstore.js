6355db86972d5c33948e3b309f3d6bfcdf24566d
Arsha <arshaskrd97@gmail.com>
2022-02-03 17:17:59 +0530

    Adding updates

--- pages/nftstore.js
+++ pages/nftstore.js
@@ -6,109 +6,113 @@ import { NextSeo } from 'next-seo'
 import Opening from '../components/Opening'
 import React from 'react'
 
+{
+  /*<div
+  className="bg-cover bg-center h-[460px] bg-gray-600"
+  style={{ backgroundImage: 'url(/c-bg.png)' }}>
+  <Menu />
+  <Masthead />
+</div>
+<section className="container my-20">
+  <div className="grid md:grid-cols-3 grid-cols-2 items-center justify-center mt-6 gap-8">
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src="/Anjing.png"
+          className="w-full rounded-lg opacity-80"
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Diamond
+      </p>
+    </div>
+
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src={`https://placeimg.com/490/500/animals`}
+          className="w-full rounded-lg opacity-80"
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Ruby
+      </p>
+    </div>
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src={`https://placeimg.com/480/500/animals`}
+          className="w-full rounded-lg opacity-80"
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Emerald
+      </p>
+    </div>
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src={`https://placeimg.com/510/500/animals`}
+          className="w-full rounded-lg opacity-80"
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Paraiba
+      </p>
+    </div>
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src={`https://placeimg.com/495/500/animals`}
+          className="w-full rounded-lg opacity-80"
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Sapphire
+      </p>
+    </div>
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src={`https://placeimg.com/490/500/animals`}
+          className="w-full rounded-lg opacity-80 "
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Aquamarine
+      </p>
+    </div>
+    <div className="bg-white p-5 rounded-lg">
+      <a href={`/nft/${new Date().getMilliseconds()}`}>
+        <img
+          src={`https://placeimg.com/485/500/animals`}
+          className="w-full rounded-lg opacity-80"
+          alt=""
+        />
+      </a>
+      <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
+        Garnet
+      </p>
+    </div>
+  </div>
+</section>
+<Footer />*/
+}
+
 const Marketplace = () => {
   return (
     <>
       <NextSeo title="Nft Store" />
 
       <main>
-        <div
-          className="bg-cover bg-center h-[460px] bg-gray-600"
-          style={{ backgroundImage: 'url(/c-bg.png)' }}>
-          <Menu />
-          <Masthead />
-        </div>
-        <section className="container my-20">
-          <div className="grid md:grid-cols-3 grid-cols-2 items-center justify-center mt-6 gap-8">
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src="/Anjing.png"
-                  className="w-full rounded-lg opacity-80"
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Diamond
-              </p>
-            </div>
-
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src={`https://placeimg.com/490/500/animals`}
-                  className="w-full rounded-lg opacity-80"
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Ruby
-              </p>
-            </div>
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src={`https://placeimg.com/480/500/animals`}
-                  className="w-full rounded-lg opacity-80"
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Emerald
-              </p>
-            </div>
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src={`https://placeimg.com/510/500/animals`}
-                  className="w-full rounded-lg opacity-80"
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Paraiba
-              </p>
-            </div>
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src={`https://placeimg.com/495/500/animals`}
-                  className="w-full rounded-lg opacity-80"
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Sapphire
-              </p>
-            </div>
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src={`https://placeimg.com/490/500/animals`}
-                  className="w-full rounded-lg opacity-80 "
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Aquamarine
-              </p>
-            </div>
-            <div className="bg-white p-5 rounded-lg">
-              <a href={`/nft/${new Date().getMilliseconds()}`}>
-                <img
-                  src={`https://placeimg.com/485/500/animals`}
-                  className="w-full rounded-lg opacity-80"
-                  alt=""
-                />
-              </a>
-              <p className="my-4 font-bold md:text-2xl text-sm text-orange pl-2 uppercase">
-                Garnet
-              </p>
-            </div>
-          </div>
-        </section>
+        <Opening />
       </main>
-      <Footer />
     </>
   )
 }
