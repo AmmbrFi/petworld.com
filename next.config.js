26a43bce3fb98ef25b9b1f982c562190cbbb8d49
Arsha <arshaskrd97@gmail.com>
2022-02-09 12:30:39 +0530

    Add api key

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ module.exports = {
     webpack5: true
   },
   publicRuntimeConfig: {
-    backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL
+    backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+    apiKey: process.env.API_KEY
   },
   target: 'serverless',
   reactStrictMode: true
